79ee4fcaaeab3e4398d4203ab53ac17cc3b68c43
Ameya Singh <singh.ameya@gmail.com>
2018-11-11 16:12:08 -0800

    Started AnagramSolver.

---
+++ AnagramSolver/AnagramSolver.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Map;
+
+public class AnagramSolver {
+
+    public AnagramSolver(List<String> dict) {
+        for (String s : dict) {
+
+        }
+    }
+
+    public void print(String s, int max) {
+
+    }
+
+    private void
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-CSE143
+**CSE143**
+
+Repo for solutions to UW CSE 143. Taken Fall 2018.
